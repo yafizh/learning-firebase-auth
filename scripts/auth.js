c02ed2335b37847cd02fbf4cb25bafd917d5193f
yafizh <a.chairil.yafizh@gmail.com>
2024-02-27 19:26:04 +0700

    logging users in

--- scripts/auth.js
+++ scripts/auth.js
@@ -20,3 +20,19 @@ logout.addEventListener('click', (e) => {
         console.log('user signed out');
     })
 });
+
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        console.log(cred.user);
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+
+});
