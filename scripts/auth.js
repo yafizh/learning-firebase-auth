969cf6d99d27a697bb0ea035c1d235a6319e365a
yafizh <a.chairil.yafizh@gmail.com>
2024-02-27 18:39:13 +0700

    creating new users

---
+++ scripts/auth.js
@@ -0,0 +1,14 @@
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+});
