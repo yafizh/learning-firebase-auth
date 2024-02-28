896a4b970d7745e55ea859dac7186db68645983d
yafizh <a.chairil.yafizh@gmail.com>
2024-02-28 20:44:37 +0700

    showing user info

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
 const guideList = document.querySelector('.guides');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details');
 
 const setupUI = (user) => {
     if (user) {
+        accountDetails.innerHTML = `<div>Logged in as ${user.email}</div>`;
+
         loggedInLinks.forEach(item => item.style.display = 'block');
         loggedOutLinks.forEach(item => item.style.display = 'none');
     } else {
+        accountDetails.innerHTML = '';
+
         loggedInLinks.forEach(item => item.style.display = 'none');
         loggedOutLinks.forEach(item => item.style.display = 'block');
     }
